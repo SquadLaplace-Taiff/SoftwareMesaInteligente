6141ebc7719c3716eee24e2ccd655f8e239d9acc
Monique Costa <79485685+monique-costa@users.noreply.github.com>
2021-08-16 22:00:22 -0300

    Projeto Spring adicionado

---
+++ API-Posicionamento/Projeto Java/taiffPosicionamento/src/main/java/br/com/senai/taiffPosicionamento/TaiffPosicionamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.senai.taiffPosicionamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaiffPosicionamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaiffPosicionamentoApplication.class, args);
+	}
+
+}
---
+++ API-Posicionamento/Projeto Java/taiffPosicionamento/src/test/java/br/com/senai/taiffPosicionamento/TaiffPosicionamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.taiffPosicionamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaiffPosicionamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
