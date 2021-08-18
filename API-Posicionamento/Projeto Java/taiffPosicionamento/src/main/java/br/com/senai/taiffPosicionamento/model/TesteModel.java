c632f0866c5a4dadfb34288cd3c2faadfec138a3
Costa Monique <monique.costa@zegna.com>
2021-08-18 20:45:09 -0300

    Correção de problemas, adicionando as models, repository's e o começo da controller teste

--- API-Posicionamento/Projeto Java/taiffPosicionamento/src/main/java/br/com/senai/taiffPosicionamento/model/TesteModel.java
+++ API-Posicionamento/Projeto Java/taiffPosicionamento/src/main/java/br/com/senai/taiffPosicionamento/model/TesteModel.java
@@ -17,37 +17,61 @@ public class TesteModel {
 	@Id 
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	private String modelo;
 	@OneToMany
 	List<CoordenadaModel> coordenadas;
 	@OneToOne
 	ZeroPecaModel zeroPeca;
 	
 	
-	public TesteModel(long id, List<CoordenadaModel> coordenadas, ZeroPecaModel zeroPeca) {
+	public TesteModel() {
 		
 	}
 	
-	public TesteModel(long id, List<CoordenadaModel> coordenadas, ZeroPecaModel zeroPeca) {
-		this.id = id;
+	
+	public TesteModel(String modelo, List<CoordenadaModel> coordenadas, ZeroPecaModel zeroPeca) {
+		this.modelo = modelo;
 		this.coordenadas = coordenadas;
 		this.zeroPeca = zeroPeca;
 	}
+
+
+
 
 	public long getId() {
 		return id;
 	}
 
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	public String getModelo() {
+		return modelo;
+	}
+
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+
 	public List<CoordenadaModel> getCoordenadas() {
 		return coordenadas;
 	}
+
 
 	public void setCoordenadas(List<CoordenadaModel> coordenadas) {
 		this.coordenadas = coordenadas;
 	}
 
+
 	public ZeroPecaModel getZeroPeca() {
 		return zeroPeca;
 	}
+
 
 	public void setZeroPeca(ZeroPecaModel zeroPeca) {
 		this.zeroPeca = zeroPeca;
