578786293e95e55343b91cf328df645ad63beb3e
Douglas-Felipe <douglasfelipe-8@hotmail.com>
2021-08-18 19:16:25 -0300

    subindo o zeroPecaModel

--- API-Posicionamento/Projeto Java/taiffPosicionamento/src/main/java/br/com/senai/taiffPosicionamento/model/ZeroPecaModel.java
+++ API-Posicionamento/Projeto Java/taiffPosicionamento/src/main/java/br/com/senai/taiffPosicionamento/model/ZeroPecaModel.java
@@ -1,5 +1,53 @@
 package br.com.senai.taiffPosicionamento.model;
 
-public class ZeroPecaModel {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="Zero_peca")
+public class ZeroPecaModel {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private Integer x;
+	private Integer y;
+	private Integer z;
+	
+	
+	public ZeroPecaModel(long id, Integer x, Integer y, Integer z) {
+		super();
+		this.id = id;
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public Integer getX() {
+		return x;
+	}
+	public void setX(Integer x) {
+		this.x = x;
+	}
+	public Integer getY() {
+		return y;
+	}
+	public void setY(Integer y) {
+		this.y = y;
+	}
+	public Integer getZ() {
+		return z;
+	}
+	public void setZ(Integer z) {
+		this.z = z;
+	}
 }
