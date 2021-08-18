af12d0d6d8dcc7478812c58d286249079e555632
RenatoMGarcia <57051641+RenatoMGarcia@users.noreply.github.com>
2021-08-17 22:42:51 -0300

    Add files via upload

---
+++ API-Posicionamento/Projeto Java/taiffPosicionamento/src/main/java/br/senai/taiffPosicionamento/model/CoordenadaModel.java
@@ -0,0 +1,83 @@
+package br.senai.taiffPosicionamento.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="coordenada")
+public class CoordenadaModel {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private int x;
+	private int y;
+	private int z;
+	private int r;
+	private int t;
+	
+	public CoordenadaModel() {
+		
+	}
+
+	public CoordenadaModel(long id, int x, int y, int z, int r, int t) {
+		
+		this.id = id;
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.r = r;
+		this.t = t;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getZ() {
+		return z;
+	}
+
+	public void setZ(int z) {
+		this.z = z;
+	}
+
+	public int getR() {
+		return r;
+	}
+
+	public void setR(int r) {
+		this.r = r;
+	}
+
+	public int getT() {
+		return t;
+	}
+
+	public void setT(int t) {
+		this.t = t;
+	}
+	
+	
+	
+	
+
+}
