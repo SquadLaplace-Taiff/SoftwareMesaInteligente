4e16645d06c11989b7bf5fd18bd351e1bdb88f32
Silas Eliaquim <eliaquimfranca08@gmail.com>
2021-08-18 21:36:30 -0300

    Teste com professor, removemos a pasta com Repository R maiusculo

--- API-Posicionamento/Projeto Java/taiffPosicionamento/src/main/java/br/com/senai/taiffPosicionamento/Repository/CoordenadaRepository.java
+++
@@ -1,5 +0,0 @@
-package br.com.senai.taiffPosicionamento.Repository;
-
-public class CoordenadaRepository {
-
-}
--- API-Posicionamento/Projeto Java/taiffPosicionamento/src/main/java/br/com/senai/taiffPosicionamento/repository/CoordenadaRepository.java
+++ API-Posicionamento/Projeto Java/taiffPosicionamento/src/main/java/br/com/senai/taiffPosicionamento/repository/CoordenadaRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.senai.taiffPosicionamento.model.CoordenadaModel;
 
-public interface CoordenadaRepository extends JpaRepository<CoordenadaModel, Long> {
-	
+public interface CoordenadaRepository extends JpaRepository<CoordenadaModel, Long>{
 	
 }
