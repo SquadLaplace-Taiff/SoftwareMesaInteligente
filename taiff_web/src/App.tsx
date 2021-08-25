d79ba2ea3a06a3eea4c4509ea7972e32fe5597d6
RenatoMGarcia <contatormg1@gmail.com>
2021-08-25 19:56:34 -0300

    Merge branch 'main' of https://github.com/SquadLaplace-Taiff/SoftwareMesaInteligente

--- taiff_web/src/App.tsx
+++ taiff_web/src/App.tsx
@@ -1,12 +1,13 @@
 import { Header } from './app/components/Header/Header';
-
 import { Search } from './app/components/Search/Search';
+import { ButtonTaiff } from './app/components/ButtonTaiff/ButtonTaiff';
 
 
 function App() {
   return (
     <section>
       <Header/>
+      <ButtonTaiff name="ASJDoaidj" /> 
       <Search/>
     </section> 
       
