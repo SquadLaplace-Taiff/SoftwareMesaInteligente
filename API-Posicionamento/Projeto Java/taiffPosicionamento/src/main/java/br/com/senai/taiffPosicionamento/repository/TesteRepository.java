aee1e41cdb28699256917029f01cdd77fff26e5a
Douglas-Felipe <douglasfelipe-8@hotmail.com>
2021-08-26 20:31:14 -0300

    correcao do front de apresetacao/atualizaçao do swagger/atualizacao no buscar pro produto

--- API-Posicionamento/Projeto Java/taiffPosicionamento/src/main/java/br/com/senai/taiffPosicionamento/repository/TesteRepository.java
+++ API-Posicionamento/Projeto Java/taiffPosicionamento/src/main/java/br/com/senai/taiffPosicionamento/repository/TesteRepository.java
@@ -1,11 +1,13 @@
 package br.com.senai.taiffPosicionamento.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.senai.taiffPosicionamento.model.TesteModel;
 
 public interface TesteRepository extends JpaRepository <TesteModel, Long> {
 
-	public TesteModel findByModelo(String modelo);
+	public List<TesteModel> findAllByModelo(String modelo);
 
 }
