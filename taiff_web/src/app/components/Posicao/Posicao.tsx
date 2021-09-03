900215a8e39e203bffcbceb96673ec9d0cb7b73f
Douglas-Felipe <douglasfelipe-8@hotmail.com>
2021-09-02 22:33:43 -0300

    Pegando valores nos formularios de posicao

--- taiff_web/src/app/components/Posicao/Posicao.tsx
+++ taiff_web/src/app/components/Posicao/Posicao.tsx
@@ -1,16 +1,24 @@
-import { render } from '@testing-library/react';
-import React from 'react';
+import { useState } from 'react';
 import { Button, FormControl, FormGroup, InputGroup, FormLabel } from 'react-bootstrap';
 import './Posicao.css';
-import { APIService } from "../../services/API.service";
 
 export function Posicao(){
+    const [value, setValue] = useState()
+    
+    function onInput(values: any) {
+        setValue(values);
+    }
+
     return(
         <div className="contenForm">
-           <FormGroup className="form">
+            <FormGroup className="form">
                 <InputGroup className="input">
                     <FormLabel className="eixo">Eixo Y</FormLabel>
-                    <FormControl className="formInput"  type="number"/>                    
+                    <FormControl 
+                        onChange={e => onInput(e.target.value)}
+                        value={value}
+                        className="formInput"  
+                        type="number"/>                    
                 </InputGroup>
                 <InputGroup className="input">
                     <FormLabel className="eixo">Eixo X</FormLabel>
@@ -28,7 +36,7 @@ export function Posicao(){
                     <FormLabel className="eixo">Eixo T</FormLabel>
                     <FormControl className="formInput" type="number"/>
                 </InputGroup>
-           </FormGroup>
+            </FormGroup>
         </div>
     );
 
