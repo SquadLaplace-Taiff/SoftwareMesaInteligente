4d5cef116f5aa9916905e14eaced2582bc047fcf
Silas Eliaquim <eliaquimfranca08@gmail.com>
2021-09-29 22:48:50 -0300

    Começado parte do front trazendo dados estaticos

--- taiff_web/src/App.tsx
+++ taiff_web/src/App.tsx
@@ -1,35 +1,17 @@
-import { Header } from './app/components/Header/Header';
-import { Search } from './app/components/Search/Search';
-import { ButtonTaiff } from './app/components/ButtonTaiff/ButtonTaiff';
-import { ButtonExcluir } from './app/components/ButtonExcluir/ButtonExcluir';
-import { Posicao } from './app/components/Posicao/Posicao';
-import { SubHeader } from './app/components/SubHeader/SubHeader';
-import { GraficoLinha } from './app/components/Graficos/GraficoLinha';
-import GraficoEstatico from './app/components/GraficoEstatico/GraficoEstatico';
+import { BrowserRouter, Route } from 'react-router-dom';
+import TelaGrafico from './app/pages/TelaGrafico';
+import TelaTabela from './app/pages/TelaTabela';
+//import { Route, BrowserRouter } from 'react-router-dom';
 
 import './style.css';
-import { teste } from './app/interfaces/interfaces';
 
 function App() {
 
-  let data: teste = {
-    modelo: 'teste',
-    nome_teste: 'nome_teste',
-    coordenadas: [ 
-      {
-        x: 3,
-        y: 3,
-        z: 3,
-        r: 4,
-        t: 5
-      }
-    ]
-  }
   return (
-    <section>
-      <GraficoEstatico />
-    </section>
-
+    <BrowserRouter>
+          <Route path="/" exact component={TelaGrafico}/>
+          <Route path="/tabela" component={TelaTabela}/> 
+    </BrowserRouter>
   );
 }
 
---
+++ taiff_web/src/app/interfaces/tabelaInterface.ts
@@ -0,0 +1,9 @@
+export type tabela = {
+    termopar_1: number,
+    termopar_2: number,
+    termopar_3: number,
+    termopar_amb: number,
+    media: number,
+    temperaturaCorrigida: number
+
+}
